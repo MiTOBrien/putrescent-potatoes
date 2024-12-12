4ea08789a6ff12b3e26e60435c80ba5c400b5c4e
Michael OBrien <obrien.michael.t@gmail.com>
2024-12-12 15:13:45 -0500

    base structure for movie poster

--- src/MoviesContainer/MoviesContainer.js
+++ src/MoviesContainer/MoviesContainer.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import MoviePoster from '../MoviePoster/MoviePoster';
 import './MoviesContainer.css';
 
 function MoviesContainer() {
+
   return (
       <section className='MoviesContainer'>
         <p>We'll make some movie posters show up here!</p>
+        <MoviePoster />
       </section>
   );
 }
