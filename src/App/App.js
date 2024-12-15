e0486a01d1f34c8a96807d367061d0b1326a6477
Michael OBrien <obrien.michael.t@gmail.com>
2024-12-14 19:28:55 -0500

    Removed commented out code

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,11 @@
 import { useState, useEffect } from 'react';
 import Header from '../Header/Header';
 import MoviesContainer from '../MoviesContainer/MoviesContainer';
-// import moviePosters from '../data/movie_posters';
 import './App.css';
 import searchIcon from '../icons/search.png';
 
 // Example imports (for later):
+// import moviePosters from '../data/movie_posters';
 // import movieDetails from '../data/movie_details';
 
 function App() {
@@ -39,34 +39,6 @@ function App() {
     })
     .catch(error => console.log(error))
   };
-
-  // const downVote = (id, vote) => {
-  //   fetch(`https://rancid-tomatillos-api.onrender.com/api/v1/movies/${id}`, {
-  //     method: "PATCH",
-  //     body: JSON.stringify(vote),
-  //     headers: {
-  //       "Content-Type": "application/json"
-  //     },
-  //   })
-  //   .then(response => response.json())
-  //   .then(posterToUpdate => {
-  //     const downvotedPoster = posters.map((poster) =>
-  //     poster.id === posterToUpdate.id ? posterToUpdate : poster
-  //   );
-  //   setPosters(downvotedPoster)
-  //   })
-  //   .catch(error => console.log(error))
-  // };
-
-  // const upVote = (id) => {
-  //   const updatePoster = posters.map((poster) => {
-  //     if (poster.id === id) {
-  //       return {...poster, vote_count: poster.vote_count += 1}
-  //     }
-  //     return poster;
-  //   })
-  //   setPosters(updatePoster);
-  // };
 
   return (
     <main className='App'>
