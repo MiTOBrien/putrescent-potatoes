5d3a800f85c5b0cf26219f56953f6af2f0e05519
Michael OBrien <obrien.michael.t@gmail.com>
2024-12-20 12:41:26 -0500

    Updated Movie Details Test file - All Tests Pass

--- cypress/e2e/movie_details.cy.js
+++ cypress/e2e/movie_details.cy.js
@@ -1,34 +1,27 @@
-import movieDetails from '../fixtures/movie_details.json';
-
 describe('Movie Details Page', () => {
-  const movie = movieDetails[0];
-
   beforeEach(() => {
-    cy.visit('http://localhost:3000/');
-
-    cy.intercept('GET', `https://rancid-tomatillos-api.onrender.com/api/v1/movies/129`, {
-      statusCode: 200,
-      body: movie,
-    }).as('getMovieDetails');
-  });
-
+    cy.visit('http://localhost:3000/389')
+  })
+    
   it('displays movie details when a movie poster is clicked', () => {
-    cy.get('.MoviePoster img').first().click(); // Click the first movie poster
+    cy.intercept('GET', `https://rancid-tomatillos-api.onrender.com/api/v1/movies/389`, {
+      statusCode: 200,
+      fixture: "movie_details"
+    })
 
-    // Assert that the image and movie details section are visible
-    cy.get('img').should('exist'); // Check if the image exists (ensure it has loaded)
-    cy.get('.MovieDetails').should('exist').within(() => {
-      cy.get('h2').should('contain', movie.title);
-      cy.get('p').should('contain', `Overview: ${movie.overview}`);
-      cy.get('p').should('contain', `Release Date: ${movie.release_date}`);
-      cy.get('p').should('contain', `Language: ${movie.original_language}`);
-      cy.get('p').should('contain', `Genres: ${movie.genre_ids.join(', ')}`);
-      
-    });
-    cy.get('.home-btn').should('exist').click();
+  // Test container and title
+  cy.get('.MovieDetails').should('exist')
+  cy.get('h1').contains('Putrescent Potatoes')
+  cy.get('.home-btn').should('exist');
 
-    cy.url().should('eq', 'http://localhost:3000/');
-
-    cy.get('.MoviesContainer').should('exist'); // Adju
+  // Assert that the image and movie details section are visible
+  cy.get('img').should('exist');
+  cy.get('.MovieDetails').should('exist').within(() => {
+    cy.get('h2').contains("12 Angry Men");
+    cy.get('p').contains("The defense and the prosecution have rested and the jury is filing into the jury room to decide if a young Spanish-American is guilty or innocent of murdering his father. What begins as an open and shut case soon becomes a mini-drama of each of the jurors' prejudices and preconceptions about the trial, the accused, and each other.");
+    cy.get('p').contains("1957-04-10");
+    cy.get('p').contains('en');
+    cy.get('p').contains('Drama');
+  });
   });
 });
